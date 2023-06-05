3f522cbd2d69ac8f28dc91c3d67d35263b7fc910
wichan7 <wichan7@naver.com>
2023-06-05 18:49:59 +0900

    modified:   app.js modified:   package-lock.json modified:   package.json modified:   routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const CryptoJS = require('crypto-js');
 const User = require('../models/user');
+require('dotenv').config();
+const { HASH_SALT } = process.env;
 
 // 회원가입
 router.post('/join', function(req, res, next) {
-  /* check */
-  if (!req.body.userId) return res.status(400).send("field 'userId' is required.");
-  if (!req.body.password) return res.status(400).send("field 'password' is required.");
+  const { userId, password } = req.body;
+
+  if (!userId) return res.status(400).send("field 'userId' is required.");
+  if (!password) return res.status(400).send("field 'password' is required.");
+
+  /**
+   * TODO: password 규칙 체크
+   */
+
+  const hashedPassword = CryptoJS.SHA256(password + HASH_SALT).toString();
 
   User.create(req.body)
-    .then( user => res.status(201).send(req.body) )
-    .catch( err => res.status(500).send(err) );
+    .then( user => res.status(201).send({userId, hashedPassword}) )
+    .catch( err => {
+      let message = "exceptional error";
+
+      if (err.code == 11000) {
+        message = "key duplicated";
+      }
+
+      res.status(500).send(message);
+    } );
 });
 
 // 로그인
