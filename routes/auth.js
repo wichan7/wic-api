7afc410085fd9239aefd88faa56c4d649a221e06
wichan7 <wichan7@naver.com>
2023-06-11 22:57:26 +0900

    add: resultCode field

--- routes/auth.js
+++ routes/auth.js
@@ -9,34 +9,39 @@ const { HASH_SALT } = process.env;
 router.post('/join', function(req, res, next) {
   const { userId, password } = req.body;
 
-  if (!userId) return res.status(400).send("field 'userId' is required.");
-  if (!password) return res.status(400).send("field 'password' is required.");
-  if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/.test(password)) return res.status(400).send("패스워드 최소 8 자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자");
+  if (!userId)
+    return res.status(400).send({resultCode: 400, resultMessage: "field 'userId' is required."});
+  if (!password)
+    return res.status(400).send({resultCode: 400, resultMessage: "field 'password' is required."});
+  if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/.test(password))
+    return res.status(400).send({resultCode: 400, resultMessage: "패스워드 최소 8 자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자"});
 
   const hashedPassword = CryptoJS.SHA256(password + HASH_SALT).toString();
 
   User.create(req.body)
-    .then( user => res.status(201).send({userId, hashedPassword}) )
+    .then( user => res.status(201).send( {resultCode: 201, result: {userId, hashedPassword}}) )
     .catch( err => {
-      let message = "exceptional error";
+      let resultMessage = "exceptional error";
 
       if (err.code == 11000) {
-        message = "key duplicated";
+        resultMessage = "key duplicated";
       }
 
-      res.status(500).send(message);
+      res.status(500).send({resultCode: 500, resultMessage});
     } );
 });
 
 // 로그인
 router.post('/login', function(req, res, next) {
   /* check */
-  if (!req.body.userId) return res.status(400).send("field 'userId' is required.");
-  if (!req.body.password) return res.status(400).send("field 'password' is required.");
+  if (!req.body.userId)
+    return res.status(400).send({resultCode: 400, resultMessage: "field 'userId' is required."});
+  if (!req.body.password)
+    return res.status(400).send({resultCode: 400, resultMessage: "field 'password' is required."});
   
   User.findOne(req.body)
-    .then( result => result ? res.send("ok") : res.status(400).send("fail") )
-    .catch( err => res.status(500).send(err) );
+    .then( result => result ? res.send({resultCode: 200, resultMessage: "ok"}) : res.status(400).send({resultCode: 400, resultMessage: "fail"}) )
+    .catch( err => res.status(500).send({resultCode: 500, resultMessage: err}) );
 });
 
 module.exports = router;
