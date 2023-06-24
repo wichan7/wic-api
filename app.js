9ee9abcd1d3c052d6da82a3290b2e52f2f597d8e
wichan7 <wichan7@naver.com>
2023-06-24 22:56:22 +0900

    추가: logger (winston) 추가: logs 디렉토리 추가 추가: NODE_ENV = production 환경에서 파일 로깅 기능 추가 추가: NODE_ENV != production 환경에서 console logging 변경: auth.js 리펙토링

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
 // set env
 require('dotenv').config();
-
 // import
 const express = require('express');
 const mongoose = require('mongoose');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-let debugI = require('debug')('app:inform');
-let debugE = require('debug')('app:error');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const logger = require('./utils/logger');
 const cors = require('cors');
-
 // define routes
 let authRoutes = require('./routes/auth');
 let chatbotRoutes = require('./routes/chatbot');
 let esdRoutes = require('./routes/esd');
 let workspaceRoutes = require('./routes/workspace');
-
 let app = express();
-
-app.use(logger('dev'));
+// logger
+app.use(morgan('dev'));
+// express extentions
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-
 /* route 설정 */
 app.use('/auth', authRoutes);
 app.use('/chatbot', chatbotRoutes);
@@ -40,7 +36,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  debugE(err);
+  logger.error(err);
   // render the error page
   res.status(err.status || 500);
   res.send(err.message);
@@ -48,8 +44,7 @@ app.use(function(err, req, res, next) {
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Successfully connected to mongodb"))
-  .catch(e => console.log(e));
-
+  .then(() => logger.info("Successfully connected to mongodb"))
+  .catch(e => logger.error(e));
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -1,55 +1,71 @@
-// get env
 require('dotenv').config();
-
+// express
 const express = require('express');
 const router = express.Router();
-let debugI = require('debug')('app:inform');
-let debugE = require('debug')('app:error');
+// utils
+const logger = require('../utils/logger');
 const CryptoJS = require('crypto-js');
+// model
 const User = require('../models/user');
 
-// 회원가입
-router.post('/join', function(req, res, next) {
-  const { userId, password } = req.body;
-
-  if (!userId)
-    return res.status(400).send({resultCode: 400, resultMessage: "field 'userId' is required."});
-  if (!password)
-    return res.status(400).send({resultCode: 400, resultMessage: "field 'password' is required."});
-  if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/.test(password))
-    return res.status(400).send({resultCode: 400, resultMessage: "패스워드 최소 8 자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자"});
-
-  const hashedPassword = CryptoJS.SHA256(password + process.env.HASH_SALT).toString();
-
-  User.create(req.body)
-    .then( user => res.status(201).send( {resultCode: 201, result: {userId, hashedPassword}}) )
-    .catch( err => {
-      let resultMessage = "exceptional error";
-
-      if (err.code == 11000) {
-        resultMessage = "key duplicated";
-      }
-
-      res.status(500).send({resultCode: 500, resultMessage});
-    } );
-});
+// codes: 2XX
+const SUCCESS_LOGIN       = { code: 200, message: "ok" };
+const SUCCESS_JOIN        = { code: 201, message: "ok" };
+// codes: 4XX
+const FAIL_LOGIN          = { code: 400, message: "로그인 정보가 존재하지 않습니다." };
+const FAIL_JOIN_DUPKEY    = { code: 400, message: "동일한 아이디가 존재합니다." };
+const WRONG_ID_RULE       = { code: 400, message: "userId 필드가 규칙에 맞지 않습니다." };
+const WRONG_PASSWORD_RULE = { code: 400, message: "password 필드가 규칙에 맞지 않습니다." };
+// codes: 5XX
+const UNEXPECTED_ERROR    = { code: 500, message: "알 수 없는 오류" };
 
 // 로그인
-router.post('/login', function(req, res, next) {
-  /* check */
-  if (!req.body.userId)
-    return res.status(400).send({resultCode: 400, resultMessage: "field 'userId' is required."});
-  if (!req.body.password)
-    return res.status(400).send({resultCode: 400, resultMessage: "field 'password' is required."});
-  
-  User.findOne(req.body)
-    .then( result => result ? res.send({resultCode: 200, resultMessage: "ok"}) : res.status(400).send({resultCode: 400, resultMessage: "fail"}) )
+router.post('/login', async function(req, res, next) {
+  const { userId, password } = req.body;
+  let result = UNEXPECTED_ERROR;
+
+  if (!userId)
+    result = WRONG_ID_RULE;
+  else if (!password)
+    result = WRONG_PASSWORD_RULE;
+  else {
+    await User.findOne({userId, password})
+    .then( user => user ? result = SUCCESS_LOGIN : result = FAIL_LOGIN )
     .catch( err => {
-      console.log(JSON.stringify(err));
-      console.log("=================");
-      console.log(err);
-      return res.status(500).send({resultCode: 500, resultMessage: err});
-     } );
+      result = UNEXPECTED_ERROR;
+      logger.error(err);
+    } );
+  }
+
+  return res.status(result.code).send(result);
+});
+
+// 회원가입
+router.post('/join', async function(req, res, next) {
+  const { userId, password } = req.body;
+  let result = UNEXPECTED_ERROR;
+
+  if (!userId)
+    result = WRONG_ID_RULE;
+  else if (!password || !/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/.test(password))
+    result = WRONG_PASSWORD_RULE;
+  else {
+    // HASHING
+    const hashedPassword = CryptoJS.SHA256(password + process.env.HASH_SALT).toString();
+    // CREATE
+    await User.create({ userId, password })
+    .then( user => result = SUCCESS_JOIN )
+    .catch( err => {
+      if (err.code == 11000) {
+        result = FAIL_JOIN_DUPKEY;
+      } else {
+        result = UNEXPECTED_ERROR;
+        logger.error(err);
+      }
+    } );
+  }
+
+  return res.status(result.code).send(result);
 });
 
 module.exports = router;
